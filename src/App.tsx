40abe3be97aa5fc20abfcf1cb396497169ed3c31
Giovanni <giovannigiampaolo58@gmail.com>
2020-12-07 16:36:18 +0100

    ci:  App customized

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <div className="w3-display-container w3-animate-opacity w3-text-white">
+            <div className="w3-display-middle">
+              <h5 className="w3-jumbo w3-animate-top">Notifica in arrivo</h5>
+            </div>
+          </div>
+
+        </header>
+      </div>
   );
 }
 
